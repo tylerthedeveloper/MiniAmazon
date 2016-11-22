628ca9ac93b78bd63f7773af2065fb0871d7ff50
Tyler Citrin <tcitrin@indiana.edu>
2016-11-22 01:31:24 -0500

    starting items

--- BuyerPage.java
+++ BuyerPage.java
@@ -8,13 +8,15 @@ public class BuyerPage extends UIPage
 	public BuyerPage()
 	{
 		super();		
-		//System.out.print(App.User.name);
 	    //setting frame specifics
 	    JButton productList = new JButton("View Products");
 	    JButton searchProducts = new JButton("Search Products");
 	    JButton viewCategories = new JButton("View Categories");
+	    UIItem itemSection = new UIItem();
 	    this.add(productList);
 	    this.add(searchProducts);
 	    this.add(viewCategories);
+	    this.add(textArea);
+	    this.add(itemSection);	    
 	}	
 }
